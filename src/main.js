a999b8993c456d494e2422f790ed235538c072ca
Niko Pitkänen <niko.pitkanen.git@outlook.com>
2025-11-02 16:29:02 +0200

    chore: refactored js from index.html to dedicated modules.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,21 @@
 // controllers task is to connect model and view.
 // model handles all the logic.
 // view handles all the updates of the ui.
+import * as game from "./modules/game.js";
 
+//setup form
+const p1Input = document.getElementById("player-1");
+const p2Input = document.getElementById("player-2");
+const gameTypeSelect = document.getElementById("game-type");
+//const legsSelect = document.getElementById("set-size");
+const startGameBtn = document.getElementById("start-game");
+startGameBtn.addEventListener("click", handleNewGame);
+
+function handleNewGame() {
+  const gameType = gameTypeSelect.value;
+  const p1 = p1Input.value;
+  const p2 = p2Input.value;
+
+  game.newGame(p1, p2, gameType);
+  console.log(game.state);
+}
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,1 +1,19 @@
-// Insert actual logic here.
+export const state = {
+  players: [
+    {
+      name: "Player1",
+      score: "301",
+    },
+    {
+      name: "Player2",
+      score: "301",
+    },
+  ],
+  gameType: 301,
+};
+
+export function newGame(p1, p2, gameType) {
+  state.gameType = parseInt(gameType, 10);
+  state.players[0] = { name: p1, score: state.gameType };
+  state.players[1] = { name: p2, score: state.gameType };
+}
