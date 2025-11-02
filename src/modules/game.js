8f71d4f01c6e33c5e66b12202c7271c8d7fa5351
Niko Pitkänen <niko.pitkanen.git@outlook.com>
2025-11-02 15:41:27 +0200

    feat: added event lister for form. added object to hold game state. added two functions: newGame injects new values to the state. handleNew assigns values for newGame function.

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,19 +1,1 @@
 // Insert actual logic here.
-const _state = {
-  players: [
-    {
-      name: "Player1",
-      score: "301",
-      legsWon: 0,
-    },
-    {
-      name: "Player2",
-      score: "301",
-      legsWon: 0,
-    },
-  ],
-  currentPlayer: 0,
-  gameType: 301,
-};
-
-function startGame(p1, p2, gameType) {}
