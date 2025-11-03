8078b9d898b9838d2189db588fa4b3c62336b63f
Niko Pitkänen <niko.pitkanen.git@outlook.com>
2025-11-03 12:46:51 +0200

    fix/empty changes in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-// To achieve modularity project uses MVC model: main.js = controller, game.js = model, ui.js = view.
-// controllers task is to connect model and view.
-// model handles all the logic.
-// view handles all the updates of the ui.
 import * as game from "./modules/game.js";
 import * as ui from "./modules/ui.js";
 
