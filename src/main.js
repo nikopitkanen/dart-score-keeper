b2594703c48e692334030c72f880f8aed55a08c8
Niko Pitkänen <niko.pitkanen.git@outlook.com>
2025-12-15 16:36:05 +0200

    feat/added undo andmain logic

--- src/main.js
+++ src/main.js
@@ -5,16 +5,23 @@ import * as ui from "./modules/ui.js";
 const p1Input = document.getElementById("player-1");
 const p2Input = document.getElementById("player-2");
 const gameTypeSelect = document.getElementById("game-type");
-//const legsSelect = document.getElementById("set-size");
+const legsSelect = document.getElementById("set-size");
 const startGameBtn = document.getElementById("start-game");
+const submitTurnBtn = document.getElementById("submit-turn");
 startGameBtn.addEventListener("click", handleNewGame);
+submitTurnBtn.addEventListener("click", handleTurn);
 
 function handleNewGame() {
   const gameType = gameTypeSelect.value;
+  const legs = legsSelect.value;
   const p1 = p1Input.value;
   const p2 = p2Input.value;
 
-  game.newGame(p1, p2, gameType);
+  game.newGame(p1, p2, gameType, legs);
   ui.scoreBoard();
-  console.log(game.state);
+  console.table(game.state);
+}
+
+function handleTurn() {
+  game.turn;
 }
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,28 +1,66 @@
 export const state = {
   players: [],
-  gameType: null,
-  currentPlayerIndex: null,
-  isGameOver: true,
+  gameType: 501,
+  legsToWin: 3,
+  currentPlayerIndex: 0,
+  isGameOver: true, // cant remember what this was for
+  history: [],
 };
 
-export function newGame(p1, p2, gameType) {
+export function newGame(p1, p2, gameType, legs) {
+  // ? VISIBILITY
   const gameSection = document.getElementById("game-section");
   const setupSection = document.getElementById("setup-section");
   gameSection.hidden = false;
   setupSection.hidden = true;
-  state.gameType = parseInt(gameType, 10);
-  const p1Legs = state.players[0] ? state.players[0].legsWon : 0;
-  const p2Legs = state.players[1] ? state.players[1].legsWon : 0;
+
+  state.gameType = Number(gameType);
+  state.legsToWin = Number(legs);
   state.players[0] = {
     name: p1,
     score: state.gameType,
-    legsWon: p1Legs,
+    legsWon: 0,
   };
   state.players[1] = {
     name: p2,
     score: state.gameType,
-    legsWon: p2Legs,
+    legsWon: 0,
   };
+
   state.currentPlayerIndex = 0;
   state.isGameOver = false;
+}
+
+export function turn(points) {
+  const currentPlayer = state.players[state.currentPlayerIndex];
+  const remainingScore = currentPlayer.score;
+  let turnScore = points;
+
+  if (turnScore > remainingScore || remainingScore - turnScore === 1) {
+    turnScore = 0;
+  }
+
+  currentPlayer.score = remainingScore - turnScore;
+
+  if (currentPlayer.score === 0) {
+    currentPlayer.legsWon += 1;
+
+    if (currentPlayer.legsWon >= state.legsToWin) {
+      console.log("winning");
+    } else {
+      console.log("game continues");
+      state.players.forEach((p) => (p.score = state.gameType));
+      state.currentPlayerIndex = (state.currentPlayerIndex + 1) % state.players.length;
+    }
+  } else {
+    state.currentPlayerIndex = (state.currentPlayerIndex + 1) % state.players.length;
+  }
+}
+
+export function undo() {
+  if (state.history.length > 0) {
+    state.history.pop();
+  } else {
+    console.log("nothing to undo");
+  }
 }
